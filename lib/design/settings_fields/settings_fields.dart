09167bad33f58e3d6b1ed799e12eba224c9e4324
Satanshu Mishra <satanshumishra@outlook.com>
2026-07-12 20:35:25 -0600

    feat: add SettingsSlider text-size control

--- lib/design/settings_fields/settings_fields.dart
+++ lib/design/settings_fields/settings_fields.dart
@@ -2,6 +2,7 @@ export 'settings_field_row.dart';
 export 'settings_section.dart';
 export 'settings_segmented.dart';
 export 'settings_select.dart';
+export 'settings_slider.dart';
 export 'settings_text_field.dart';
 export 'settings_time_field.dart';
 export 'settings_toggle.dart';
