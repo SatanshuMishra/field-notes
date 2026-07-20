5872ef0552d8d3be9df3cc157adc2b8f7396115b
Satanshu Mishra <satanshumishra@outlook.com>
2026-07-20 16:12:53 -0600

    feat: day-detail public barrel

---
+++ lib/features/day_detail/day_detail.dart
@@ -0,0 +1,8 @@
+export 'day_detail_edit_note.dart';
+export 'day_detail_entries_bar.dart';
+export 'day_detail_entry_tile.dart';
+export 'day_detail_header.dart';
+export 'day_detail_heading.dart';
+export 'day_detail_panel.dart';
+export 'day_detail_providers.dart';
+export 'show_day_detail.dart';
